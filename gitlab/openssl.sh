0da2c5ffbc8c6546e212a22a6f169e9280fdb3c4
VemComPY <rafabios@users.noreply.github.com>
2020-08-22 11:59:21 -0300

    removido virgula do CN

--- gitlab/openssl.sh
+++ gitlab/openssl.sh
@@ -11,5 +11,5 @@ cd ssl/
 openssl genrsa -out server.key 2048
 openssl rsa -in server.key -out server.key
 # Modificar linha abaixo!
-openssl req -sha256 -new -key server.key -out server.csr -subj '/CN=gitlab.vemcompy.net,'
+openssl req -sha256 -new -key server.key -out server.csr -subj '/CN=gitlab.vemcompy.net'
 openssl x509 -req -sha256 -days 365 -in server.csr -signkey server.key -out server.crt
