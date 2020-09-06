95e81f9fef0481d03f76d83ca062d709eb7d07b8
VemComPY <rafabios@users.noreply.github.com>
2020-09-06 10:36:36 -0300

    Create soma_doctstring.py

---
+++ soma_doctstring.py
@@ -0,0 +1,18 @@
+import sys
+
+## Para chamar no console
+## python
+## from soma_doctstring import soma
+## help(soma)
+
+#a = int(sys.argv[1])
+#b = int(sys.argv[2])
+
+def soma(a,b):
+    """ Soma de inteiros, informe dois parametros para essa funcao, necessita ser int()  """
+    return a + b
+
+
+#result = soma(a,b)
+
+#print(result)
